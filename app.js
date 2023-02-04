14ecf2ba40e6362369a5ebca7290b96310016863
Carlit060 <cfranco37@student.alamo.edu>
2023-02-03 19:54:09 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log("hey do you have any pets ?")
+//console.log("hey do you have any pets ?")
+//2.4 assigment//
+
+var life = 100 ; // this is my life bar NUMBER
+
+life = life -10;
+
+//90
+
+life = life / 10;
+
+var name = "Carlos Franco"; // this is the name displayed in the game STRING
+
+var checkout = false; // BOOLEAN
+
+var box = null;
+
+// const life = 100; cant be re declared 
+
+// let life = 100 it can be changed with
+// life = life - 50;
+// const usually used for images
+
+
+console.log(box);
